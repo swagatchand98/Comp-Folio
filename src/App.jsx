6109a12bc0f86f56cc9500e7b4a7cdaf879844df
Swagat Chand <swagatsaswatchand@gmail.com>
2025-02-08 04:40:14 +0530

    Added displayContent file and used contextAPI for data sharing , problem - doulble rendering of the card array

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './Screen/home'
 function App() {
  
     return <>
-
            <Home/>
     </>
 }
--- src/Component/contentSlider.jsx
+++ src/Component/contentSlider.jsx
@@ -1,15 +1,42 @@
+import { memo, useContext, useEffect } from "react";
 import DisplayCard from "./displayCard"
+import { ContentContextProvider, ContentContext } from "./displayContent";
 
 const ContentSlider = () => {
+    return (
+        <ContentContextProvider>
+            <Content />
+        </ContentContextProvider>
+    );
+};
 
-    return <div className="grid grid-cols-2 grid-rows-2 gap-x-10 pt-5 px-5 bg-radial from-stone-950 from-75% to-black h-screen w-390">
+const Content = memo(() => {
 
-        <DisplayCard compPreview={"card comp"} Title={"Card"} Description={"This is a Card Component with float effect on Hover"}/>
-        <DisplayCard compPreview={"card comp"} Title={"Card"} Description={"This is a Card Component with float effect on Hover"}/>
-        <DisplayCard compPreview={"card comp"} Title={"Card"} Description={"This is a Card Component with float effect on Hover"}/>
-        <DisplayCard compPreview={"card comp"} Title={"Card"} Description={"This is a Card Component with float effect on Hover"}/>
+    const { cards } = useContext(ContentContext);
 
+    useEffect(() => {
+        // Add render count to track component lifecycle
+        const timestamp = new Date().toISOString();
+        console.log(`[${timestamp}] Content mounted with ${cards?.length} cards`);
+        
+        return () => {
+            console.log(`[${timestamp}] Content unmounted`);
+        };
+    }, []);
+    
+    return (
+    <div className="grid grid-cols-2 grid-rows-2 gap-x-10 pt-5 px-5 bg-radial from-stone-950 from-75% to-black h-screen w-390">
+
+     {cards.map((card) => (
+                <DisplayCard
+                    key={card.id}
+                    compPreview={card.compPreview}
+                    Title={card.Title}
+                    Description={card.Description}
+                />
+            ))}    
     </div>
-}
+    );
+})
 
 export default ContentSlider
