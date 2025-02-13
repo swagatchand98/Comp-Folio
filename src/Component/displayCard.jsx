cccdeb0dd786f70b460336dff3702e3b5f45da99
Swagat Chand <swagatsaswatchand@gmail.com>
2025-02-13 16:23:42 +0530

    Added hover effect to displayCard

--- src/Component/displayCard.jsx
+++ src/Component/displayCard.jsx
@@ -1,8 +1,8 @@
 const DisplayCard = ({compPreview, Title, Description}) => {
 
-    return <div className="h-60 md:h-100 px-3 bg-[#130F0F]">
-        <div className="flex justify-center items-center pt-5">
-           <div className="w-screen h-40 md:h-80 bg-[#2F2F2F] flex justify-center items-center">
+    return <div className="h-60 md:h-100 px-3 bg-[#130F0F]   hover:border-1 hover:border-zinc-500 hover:shadow-[0px_0px_20px_5px_rgba(255,255,255,0.1)] active:border-1 active:border-zinc-500 active:shadow-[0px_0px_20px_5px_rgba(255,255,255,0.2)]">
+        <div className="flex justify-center items-center pt-5 cursor-pointer">
+           <div className="w-screen h-40 md:h-80 bg-[#2F2F2F] flex justify-center items-center ">
                {compPreview}
            </div>
         </div>   
@@ -13,7 +13,7 @@ const DisplayCard = ({compPreview, Title, Description}) => {
                <div>
                    {Description}
                </div>
-           </div>    
+           </div>
     </div>
 }
 
